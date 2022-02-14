d3f5771075050ebfbbe1ca331c207abc897f1b34
zhaoyongqi2022 <yongqizhao5@gmail.com>
2022-02-14 19:38:24 +0100

    add LeetCode 70

---
+++ DP：70爬楼梯.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#include<vector>
+#include<algorithm>
+
+int climbStairs(int n) {
+	if (n <= 2) return n;
+
+	vector<int> dp(n + 1);
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+}
+
+//int main()
+//{
+//
+//
+//	system("pause");
+//	return 0;
+//}
